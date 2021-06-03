cb801a074b1a6fabc2e788f892558551c5540f89
Dorian Oszczęda <vxern@ctemplar.com>
2021-06-03 12:02:01 +0100

    Initial commit

---
+++ example/spidart_example.dart
@@ -0,0 +1,6 @@
+import 'package:spidart/spidart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/spidart.dart
@@ -0,0 +1,3 @@
+library spidart;
+
+export 'src/crawler.dart';
