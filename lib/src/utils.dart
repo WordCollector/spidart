a4e1704be9fcdd7db3fc6c4393c76a76eeac1865
Dorian Oszczęda <vxern@ctemplar.com>
2021-06-06 23:47:15 +0100

    Add url helper functions to the Utils class

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,5 +1,21 @@
 extension TextExtraction on String {
+  /// Extract the parts of text that correspond with the pattern
   Iterable<String> extract(Pattern pattern) {
     return pattern.allMatches(this).map((m) => m.group(0)!.replaceAll('"', ''));
+  }
+}
+
+class Utils {
+  /// Remove path to leave just the hostname
+  static String getHostFromUrl(String url) => url.split('/').sublist(0, 3).join('/');
+
+  /// Remove hostname to leave just the path
+  static String getPathFromUrl(String url) => '/' + url.split('/').sublist(3).join('/');
+
+  /// Remove the final part of the path to leave just the parent directory
+  static String getParentPath(String url) {
+    final parts = url.split('/');
+    parts.removeLast();
+    return parts.join('/') + '/';
   }
 }
